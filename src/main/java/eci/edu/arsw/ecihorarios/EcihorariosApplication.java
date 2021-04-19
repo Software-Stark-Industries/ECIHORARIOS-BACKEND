5f6cfe4ace9b70cb4a24391a7c26af48c9c9d3ac
FabiMauri47 <fabian.ramirez-p@mail.escuelaing.edu.co>
2021-04-19 15:09:57 -0500

    Spring initializer

---
+++ src/main/java/eci/edu/arsw/ecihorarios/EcihorariosApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.arsw.ecihorarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcihorariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcihorariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/arsw/ecihorarios/EcihorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.arsw.ecihorarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcihorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
